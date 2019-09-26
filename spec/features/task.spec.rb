96ccb6e1f8592036f0de1a80423ffaec75989560
Aimable <aimatwiri@gmail.com>
2019-09-26 12:32:47 +0200

    correction

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -6,19 +6,21 @@ RSpec.feature "Task management function", type: :feature do
     FactoryBot.create(:task, title: 'task2', content: 'content2')
     FactoryBot.create(:task, title: 'task3', content: 'content3')
     FactoryBot.create(:task, title: 'task4', content: 'content4')
-    visit tasks_path
-  save_and_open_page
+    
   end
+  
   scenario "Test task list" do
-  
-  
-  expect(page).to have_content 'content1'
-  expect(page).to have_content 'content2'
+    visit tasks_path
+    save_and_open_page
+    expect(page).to have_content 'content1'
+    expect(page).to have_content 'content2'
   end
   
   #omitted
   scenario "Test whether tasks are arranged in descending order of creation date" do
     Task.all.order('created_at desc')
+    visit tasks_path
+    save_and_open_page
     Task.order('created_at desc').all.should == Task.all.order('created_at desc')
   end
 end
