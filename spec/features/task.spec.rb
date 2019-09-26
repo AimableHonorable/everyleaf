67ab1bba782e4102f27926318dbbb2e8a432ca56
Aimable <aimatwiri@gmail.com>
2019-09-26 14:28:55 +0200

    merger master to my branch

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -16,11 +16,18 @@ RSpec.feature "Task management function", type: :feature do
     expect(page).to have_content 'content2'
   end
   
-  #omitted
+  scenario "Test task creation" do
+    visit new_task_path
+    fill_in 'Title', with: 'greeting'
+    fill_in 'Content', with: 'good'
+    click_button '登録する'
+    save_and_open_page
+    expect(page).to have_content 'good'
+  end
+
   scenario "Test whether tasks are arranged in descending order of creation date" do
     Task.all.order('created_at desc')
     visit tasks_path
     save_and_open_page
-    Task.order('created_at desc').all.should == Task.all.order('created_at desc')
   end
 end
