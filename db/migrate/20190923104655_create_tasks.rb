3288ccb0ec0abdc8637708817b147ae1c63166e3
Aimable <aimatwiri@gmail.com>
2019-09-26 13:56:48 +0200

    correction

--- db/migrate/20190923104655_create_tasks.rb
+++ db/migrate/20190923104655_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.text :title, null
+      t.text :title
       t.text :content
       t.timestamps
     end
