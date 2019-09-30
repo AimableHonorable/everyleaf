a963a42ae35ca2abd436d81ae546b5504a7d0565
Aimable <aimatwiri@gmail.com>
2019-09-30 09:01:11 +0200

    added a function to register the deadline of the task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_120619) do
+ActiveRecord::Schema.define(version: 2019_09_30_064437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_09_25_120619) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_at"
+    t.datetime "end_at"
   end
 
 end
