7d012a54fe6f2688c28549d89cb2a28b43f8f125
Aimable <aimatwiri@gmail.com>
2019-09-30 10:26:42 +0200

    added a test of order by deadline

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.feature "Task management function", type: :feature do
   background do
-    FactoryBot.create(:task, title: 'task1', content: 'content1')
-    FactoryBot.create(:task, title: 'task2', content: 'content2')
-    FactoryBot.create(:task, title: 'task3', content: 'content3')
-    FactoryBot.create(:task, title: 'task4', content: 'content4')
+    FactoryBot.create(:task, title: 'task1', content: 'content1', start_at: '2019-10-01', end_at: '2019-10-02')
+    FactoryBot.create(:task, title: 'task2', content: 'content2', start_at: '2019-10-03', end_at: '2019-10-04')
+    FactoryBot.create(:task, title: 'task3', content: 'content3', start_at: '2019-10-05', end_at: '2019-10-06')
+    FactoryBot.create(:task, title: 'task4', content: 'content4', start_at: '2019-10-07', end_at: '2019-10-08')
     
   end
   
@@ -30,4 +30,12 @@ RSpec.feature "Task management function", type: :feature do
     visit tasks_path
     save_and_open_page
   end
+
+  scenario "Test if tasks are ordered by deadline" do
+    visit tasks_path
+    click_button 'Sort by deadline'
+    assert Task.all.order('end_at desc')
+    save_and_open_page
+  end
+
 end
