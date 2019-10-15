ce70cdbebb20ea2d81cc0627609a293045615143
Aimable <aimatwiri@gmail.com>
2019-10-15 12:14:29 +0200

    created login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :tasks
+  validates :firstname , presence:true
+  validates :lastname , presence:true
+  validates :email , presence:true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
 end
