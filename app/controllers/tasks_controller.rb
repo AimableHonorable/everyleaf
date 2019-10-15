d5f6fc5b667e61303aefcb1e7d88c14bbb89a178
Aimable <aimatwiri@gmail.com>
2019-10-15 14:57:59 +0200

    display only tasks that you created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,13 +4,13 @@ class TasksController < ApplicationController
   def index
     @search = Task.ransack(params[:q])
     if params[:q]
-      @tasks = @search.result.page(params[:page]).per(2)
+      @tasks = @search.result.page(params[:page])
     elsif params[:sort_deadline]
-      @tasks = Task.all.order('end_at DESC').page(params[:page]).per(2)
+      @tasks = Task.all.order('end_at DESC').page(params[:page])
     elsif params[:sort_priority]
-      @tasks = Task.all.order('priority DESC').page(params[:page]).per(2)
+      @tasks = Task.all.order('priority DESC').page(params[:page])
     else
-      @tasks = Task.all.order('created_at DESC').page(params[:page]).per(2)
+      @tasks = Task.all.order('created_at DESC').page(params[:page])
     end
   end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ApplicationRecord
   validates :title, presence:true
   validates :content, presence:true
   enum priority: [:low, :medium, :high]
+  paginates_per 2
 end
