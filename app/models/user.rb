f3018ce6df873691818b57a06151d8eb58bb5ae0
Aimable <aimatwiri@gmail.com>
2019-10-22 23:23:02 +0200

    delete users  with their corresponding tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   validates :firstname , presence:true
   validates :lastname , presence:true
   validates :email , presence:true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
