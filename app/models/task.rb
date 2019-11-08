86cb5eaf70a77d9632b969ea38553523e2e9a514
Aimable <aimatwiri@gmail.com>
2019-11-08 14:24:44 +0200

    correction

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,6 @@ class Task < ApplicationRecord
   validates :title, presence:true
   validates :content, presence:true
   enum priority: [:low, :medium, :high]
-  paginates_per 2
   has_many :task_labels, dependent: :destroy
   has_many :labels, through: :task_labels
 end
